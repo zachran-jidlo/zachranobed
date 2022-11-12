c620405ccb05de3e8f942eb18c3eaba07a55ab48
Jakub Timko <xtimko1@fi.muni.cz>
2022-11-12 17:26:33 +0100

    Changing text field style

--- lib/ui/widgets/textField.dart
+++ lib/ui/widgets/textField.dart
@@ -15,18 +15,26 @@ class ZachranObedTextField extends StatefulWidget {
 }
 
 class _ZachranObedTextFieldState extends State<ZachranObedTextField> {
+
+  final TextFieldBorder = const OutlineInputBorder(
+    borderSide: BorderSide(
+      width: 2,
+      color: Colors.black,
+    ),
+    borderRadius: BorderRadius.zero,
+  );
+
   @override
   Widget build(BuildContext context) {
     return TextField(
       obscureText: widget.obscureText,
+      cursorColor: Colors.black,
       decoration: InputDecoration(
         labelText: widget.text,
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-              width: 2
-          ),
-          borderRadius: BorderRadius.zero,
-        ),
+        labelStyle: TextStyle(color: Colors.grey[600]),
+        enabledBorder: TextFieldBorder,
+        focusedBorder: TextFieldBorder,
+        focusColor: Colors.redAccent,
       ),
     );
   }
