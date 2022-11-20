698a7eb71f2aed64d4b5529bb4741206fc430493
Jakub Timko <xtimko1@fi.muni.cz>
2022-11-20 19:59:52 +0100

    Adding list of most recently donated meals to the home screen

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -5,6 +5,7 @@ import 'package:zachranobed/custom_icons_icons.dart';
 import 'package:zachranobed/models/user.dart';
 import 'package:zachranobed/routes.dart';
 import 'package:zachranobed/ui/widgets/card.dart';
+import 'package:zachranobed/ui/widgets/listTile.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -30,57 +31,92 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              color: Colors.black,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Dnes můžete darovat ještě 23 h : 59 min',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.black,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ZachranObedCard(
-                    text: ZachranObedStrings.savedLunches,
-                    measuredVariableText: '672',
-                    buttonText: ZachranObedStrings.statistics,
-                    onPressed: () {
-                      print("Jdu na statistiky");
-                    },
-                  ),
-                  ZachranObedCard(
-                      text: ZachranObedStrings.borrowedBoxes,
-                      measuredVariableText: '32/100',
-                      buttonText: ZachranObedStrings.change,
-                      onPressed: () {
-                        print("Jdu změnit počet krabiček");
-                      },
+                  Text(
+                    'Dnes můžete darovat ještě 23 h : 59 min',
+                    style: TextStyle(color: Colors.white),
                   ),
                 ],
               ),
             ),
+          ),
+          const SizedBox(height: 10),
 
-            Consumer<User>(
-              builder: (context, user, child) {
-                return Text('Ahoj ${user.email}');
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Row(
+              children: [
+                ZachranObedCard(
+                  text: ZachranObedStrings.savedLunches,
+                  measuredVariableText: '672',
+                  buttonText: ZachranObedStrings.statistics,
+                  onPressed: () {
+                    print("Jdu na statistiky");
+                  },
+                ),
+                ZachranObedCard(
+                    text: ZachranObedStrings.borrowedBoxes,
+                    measuredVariableText: '32/100',
+                    buttonText: ZachranObedStrings.change,
+                    onPressed: () {
+                      print("Jdu změnit počet krabiček");
+                    },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 15),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Container(
+              alignment: Alignment.centerLeft,
+              child: Text('Naposledy darováno'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: ZachranObedListTile(
+              text: '31.12. Název pokrmu 00 ks',
+              onTapped: () {
+                print("Kliknuto na darovaný pokrm");
               },
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: ZachranObedListTile(
+              text: '31.12. Název pokrmu 00 ks',
+              onTapped: () {
+                print("Kliknuto na darovaný pokrm");
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: ZachranObedListTile(
+              text: '31.12. Název pokrmu 00 ks',
+              onTapped: () {
+                print("Kliknuto na darovaný pokrm");
+              },
+            ),
+          ),
+
+          const SizedBox(height: 50),
+          Consumer<User>(
+            builder: (context, user, child) {
+              return Text('Ahoj ${user.email}');
+            },
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
