3df205ec22ba7a14e7a08ca1dd267c23332c5a86
Jakub Timko <xtimko1@fi.muni.cz>
2023-07-15 09:58:47 +0200

    Introduce firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:zachranobed/firebase/firebase_options.dart';
 import 'package:zachranobed/ui/widgets/app_root.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const AppRoot());
 }
