5cfcc6fbdc77ec959d66225b09ac88a3571672c7
Jakub Timko <xtimko1@fi.muni.cz>
2023-09-05 22:30:25 +0200

    Minor fixes

--- lib/ui/widgets/snackbar/persistent_snackbar.dart
+++ lib/ui/widgets/snackbar/persistent_snackbar.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:zachranobed/shared/constants.dart';
 
-double _HEIGHT = 48.0;
-
 class ZOPersistentSnackBar extends StatelessWidget {
   final String message;
 
@@ -10,32 +8,36 @@ class ZOPersistentSnackBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-      height: _HEIGHT,
-      decoration: ShapeDecoration(
-        color: ZOColors.infoSnackBarBackground,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-        shadows: const [
-          BoxShadow(
-            color: Color(0x4C000000),
-            blurRadius: 3,
-            offset: Offset(0, 1),
-            spreadRadius: 0,
+    return Wrap(
+      children: [
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+          decoration: ShapeDecoration(
+            color: ZOColors.infoSnackBarBackground,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+            shadows: const [
+              BoxShadow(
+                color: Color(0x4C000000),
+                blurRadius: 3,
+                offset: Offset(0, 1),
+                spreadRadius: 0,
+              ),
+              BoxShadow(
+                color: Color(0x26000000),
+                blurRadius: 8,
+                offset: Offset(0, 4),
+                spreadRadius: 3,
+              )
+            ],
           ),
-          BoxShadow(
-            color: Color(0x26000000),
-            blurRadius: 8,
-            offset: Offset(0, 4),
-            spreadRadius: 3,
-          )
-        ],
-      ),
-      child: Text(
-        message,
-        style: const TextStyle(color: ZOColors.onInfoSnackBarBackground),
-      ),
+          child: Text(
+            message,
+            style: const TextStyle(color: ZOColors.onInfoSnackBarBackground),
+          ),
+        ),
+      ],
     );
   }
 }
