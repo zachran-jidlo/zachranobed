603640e8f3ae6cf72f043e9585ad877be13eaa78
Jakub Timko <xtimko1@fi.muni.cz>
2023-09-07 19:05:39 +0200

    Introduce box movement detail screen

--- lib/ui/screens/box_movement_detail_screen.dart
+++ lib/ui/screens/box_movement_detail_screen.dart
@@ -1,6 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:zachranobed/extensions/build_context_extensions.dart';
 import 'package:zachranobed/models/box_movement.dart';
+import 'package:zachranobed/shared/constants.dart';
+import 'package:zachranobed/ui/widgets/snackbar/persistent_snackbar.dart';
+import 'package:zachranobed/ui/widgets/supporting_text.dart';
+import 'package:zachranobed/ui/widgets/text_field.dart';
 
 @RoutePage()
 class BoxMovementDetailScreen extends StatelessWidget {
@@ -12,6 +18,41 @@ class BoxMovementDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: WidgetStyle.padding,
+          ),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text(
+                    boxMovement.boxType,
+                    style: const TextStyle(fontSize: FontSize.l),
+                  ),
+                ],
+              ),
+              const SizedBox(height: GapSize.s),
+              ZOTextField(
+                label: context.l10n!.numberOfBoxes,
+                initialValue: boxMovement.numberOfBoxes.toString(),
+                readOnly: true,
+              ),
+              const SizedBox(height: GapSize.xs),
+              SupportingText(
+                text: '${context.l10n!.sentOn}'
+                    ' ${DateFormat('d.M.y').format(boxMovement.date)}'
+                    ' ${context.l10n!.atTime}'
+                    ' ${DateFormat('HH:mm').format(boxMovement.date)}.',
+              ),
+              const SizedBox(height: GapSize.xs),
+              ZOPersistentSnackBar(message: context.l10n!.formCantBeEdited),
+              const SizedBox(height: GapSize.s),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
