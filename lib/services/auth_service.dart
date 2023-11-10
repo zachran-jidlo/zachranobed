9e89b7d5a3651d76300ca1e6baf661df69fd9f4d
Jakub Timko <xtimko1@fi.muni.cz>
2023-11-10 12:06:51 +0100

    Document methods of AuthService

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -10,6 +10,11 @@ class AuthService {
 
   AuthService(this._canteenService, this._charityService);
 
+  /// Returns data about currently signed in user.
+  ///
+  /// Retrieves the claims of the currently signed in user and, based on
+  /// whether they contain the `canteen` or `charity` attribute, retrieves
+  /// data from the appropriate Firestore collection.
   Future<UserData?> getUserData() async {
     final token = await _auth.currentUser!.getIdTokenResult(false);
     final claims = token.claims;
@@ -23,6 +28,10 @@ class AuthService {
     return null;
   }
 
+  /// Attempts to sign user to the app with given [email] and [password].
+  ///
+  /// If successful returns a [Future] that completes with a [User] object and
+  /// `null` if there is an error during the sign-in process.
   Future<User?> signIn(String email, String password) async {
     try {
       UserCredential result = await _auth.signInWithEmailAndPassword(
@@ -35,10 +44,12 @@ class AuthService {
     }
   }
 
+  /// Signs out the current user.
   Future<void> signOut() async {
     await _auth.signOut();
   }
 
+  /// Re-authenticates user with a given [password].
   Future<void> reauthenticateUser(String password) async {
     final credential = EmailAuthProvider.credential(
       email: _auth.currentUser!.email!,
@@ -48,10 +59,12 @@ class AuthService {
     await _auth.currentUser!.reauthenticateWithCredential(credential);
   }
 
+  /// Updates the user's password with the new provided [password].
   Future<void> changePassword(String password) async {
     await _auth.currentUser!.updatePassword(password);
   }
 
+  /// Sends a password reset email to the provided [email] address.
   Future<void> resetPassword(String email) async {
     await _auth.sendPasswordResetEmail(email: email);
   }
