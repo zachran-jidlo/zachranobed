df78f4e0b6653fd9cd4f075f4175a56fc3474c22
Jakub Timko <xtimko1@fi.muni.cz>
2023-11-16 12:35:48 +0100

    Document AuthGuard method

--- lib/routes/guards/auth_guard.dart
+++ lib/routes/guards/auth_guard.dart
@@ -3,6 +3,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:zachranobed/routes/app_router.gr.dart';
 
 class AuthGuard extends AutoRouteGuard {
+  /// Determines whether the user is authenticated during the navigation.
+  /// If the current user is authenticated, the navigation proceeds. If not
+  /// authenticated, it replaces the current route stack with a new stack
+  /// containing the [LoginRoute].
   @override
   void onNavigation(NavigationResolver resolver, StackRouter router) {
     if (FirebaseAuth.instance.currentUser != null) {
