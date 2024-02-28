364ce33affd2440c0489af9a0a2e597c806cf18e
Oleksandr Balan <oleksandr.balan@etnetera.cz>
2024-02-28 19:47:29 +0100

    ZOB-90 - Extract end part of menu item, adjust paddings

--- lib/ui/widgets/menu/menu_item.dart
+++ lib/ui/widgets/menu/menu_item.dart
@@ -29,34 +29,43 @@ class MenuItem extends StatelessWidget {
             borderRadius: BorderRadius.circular(8),
           ),
           child: Padding(
-            padding: secondaryText == null
-                ? const EdgeInsets.symmetric(horizontal: WidgetStyle.padding)
-                : const EdgeInsets.only(right: WidgetStyle.padding),
+            padding:
+                const EdgeInsets.symmetric(horizontal: WidgetStyle.padding),
             child: Row(
               children: [
                 if (leadingIcon != null) ...[
                   Icon(leadingIcon),
                   const SizedBox(width: GapSize.xs),
                 ],
-                const SizedBox(width: GapSize.xs),
                 Text(text, style: const TextStyle(fontSize: FontSize.s)),
                 const Spacer(),
-                secondaryText != null
-                    ? Text(secondaryText!,
-                        style: const TextStyle(fontSize: FontSize.s))
-                    : onPressed != null
-                        ? const Icon(
-                            Icons.arrow_forward_ios,
-                            size: 16,
-                          )
-                        : const SizedBox(),
+                _endPart(),
               ],
             ),
           ),
         ),
       );
     } else {
+      // Return an empty SizedBox when not visible
       return const SizedBox();
-    } // Return an empty SizedBox when not visible
+    }
+  }
+
+  Widget _endPart() {
+    if (secondaryText != null) {
+      return Text(
+        secondaryText!,
+        style: const TextStyle(fontSize: FontSize.s),
+      );
+    }
+
+    if (onPressed != null) {
+      return const Icon(
+        Icons.arrow_forward_ios,
+        size: 16,
+      );
+    }
+
+    return const SizedBox();
   }
 }
