88aa648dbd7e901df11f802514a25c02706ee198
Oleksandr Balan <oleksandr.balan@etnetera.cz>
2024-03-13 12:57:06 +0100

    ZOB-114 - Remove JsonSerializable unused annotation

--- lib/models/charity.dart
+++ lib/models/charity.dart
@@ -1,13 +1,5 @@
-import 'package:json_annotation/json_annotation.dart';
 import 'package:zachranobed/models/user_data.dart';
 
-/*
- * Command to rebuild the charity.g.dart file:
- * flutter packages pub run build_runner build --delete-conflicting-outputs
- */
-part 'charity.g.dart';
-
-@JsonSerializable()
 class Charity extends UserData {
   final List<String>? donorId;
 
@@ -18,9 +10,4 @@ class Charity extends UserData {
     required super.organization,
     this.donorId,
   });
-
-  factory Charity.fromJson(Map<String, dynamic> json) =>
-      _$CharityFromJson(json);
-
-  Map<String, dynamic> toJson() => _$CharityToJson(this);
 }
