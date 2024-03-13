051cf3766b818b5e07380da8739f5352d92e52aa
Oleksandr Balan <oleksandr.balan@etnetera.cz>
2024-03-13 12:57:06 +0100

    ZOB-114 - Remove JsonSerializable unused annotation

--- lib/models/canteen.dart
+++ lib/models/canteen.dart
@@ -1,13 +1,5 @@
-import 'package:json_annotation/json_annotation.dart';
 import 'package:zachranobed/models/user_data.dart';
 
-/*
- * Command to rebuild the canteen.g.dart file:
- * flutter packages pub run build_runner build --delete-conflicting-outputs
- */
-part 'canteen.g.dart';
-
-@JsonSerializable()
 class Canteen extends UserData {
   final String? pickUpFrom;
   final String? pickUpWithin;
@@ -22,9 +14,4 @@ class Canteen extends UserData {
     this.pickUpWithin,
     this.recipientId,
   });
-
-  factory Canteen.fromJson(Map<String, dynamic> json) =>
-      _$CanteenFromJson(json);
-
-  Map<String, dynamic> toJson() => _$CanteenToJson(this);
 }
