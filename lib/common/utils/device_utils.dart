35029ec5db6af329b43e48eeb18c54fd52e93746
Oleksandr Balan <oleksandr.balan@etnetera.cz>
2024-03-25 20:51:57 +0100

    ZOB-114 - Fix review

--- lib/common/utils/device_utils.dart
+++ lib/common/utils/device_utils.dart
@@ -1,18 +1,19 @@
 import 'dart:io';
+
 import 'package:android_id/android_id.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 
 /// Device related utility functions.
 class DeviceUtils {
-
   /// Retrieves device ID for Android or iOS.
   static Future<String?> getId() async {
-    if (Platform.isIOS) {
-      final info = await DeviceInfoPlugin().iosInfo;
-      return info.identifierForVendor;
-    } else if (Platform.isAndroid) {
-      return await const AndroidId().getId();
+    switch (Platform.operatingSystem) {
+      case 'ios':
+        return (await DeviceInfoPlugin().iosInfo).identifierForVendor;
+      case 'android':
+        return await const AndroidId().getId();
+      default:
+        return null;
     }
-    return null;
   }
 }
