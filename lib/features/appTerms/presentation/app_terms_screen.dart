bbe39f1b76f97dbe3f70985a228a2b5da65fc2cc
Jaroslav Novák <97612338+JaroslavNovak9@users.noreply.github.com>
2024-04-16 14:35:51 +0200

    ZOB-141 | Visual implementation (#88)
    * ZOB-114 - Fetch used data from entities and entity pairs collections
    
    * ZOB-114 - Remove JsonSerializable unused annotation
    
    * ZOB-114 - Update changelog
    
    * ZOB-125 - Change FCM tokens location
    
    * ZOB-125 - Update changelog
    
    * feat:wip - Prepared foundation of check UC.
    
    * feat - Added basic redirect to screen to test, if the logic works in the future commits
    
    * feat - Partially connected displayment logic
    
    * feat - Updated user entity
    
    * feat - wip
    
    * feat - Added entity updated and will enter foreground state reaction
    
    * feat - Added value fetching. Completed feature
    
    * feat - Added visuals for app terms screen
    
    * feat - PRocessed CR notes
    
    * feat - Added widget observer and fixed the issue with logged in user
    
    * fix - Processed CR issue
    
    * feat - Corrected changelog
    
    * feat - Processed CR
    
    ---------
    
    Co-authored-by: Oleksandr Balan <oleksandr.balan@etnetera.cz>

--- lib/features/appTerms/presentation/app_terms_screen.dart
+++ lib/features/appTerms/presentation/app_terms_screen.dart
@@ -1,5 +1,12 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_material_symbols/flutter_material_symbols.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get_it/get_it.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:zachranobed/common/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_material_symbols/flutter_material_symbols.dart';
 import 'package:get_it/get_it.dart';
@@ -7,6 +14,8 @@ import 'package:zachranobed/features/appTerms/domain/set_newest_accepted_app_ter
 import 'package:zachranobed/ui/widgets/button.dart';
 import 'package:zachranobed/routes/app_router.gr.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:zachranobed/ui/widgets/checkbox.dart';
+import 'package:zachranobed/ui/widgets/snackbar/temporary_snackbar.dart';
 
 @RoutePage()
 class AppTermsScreen extends StatefulWidget {
@@ -18,34 +27,93 @@ class AppTermsScreen extends StatefulWidget {
 
 class _AppTermsScreen extends State<AppTermsScreen> {
   final _setNewestAcceptedAppTermsUseCase = GetIt.I<SetNewestAcceptedAppTermsUseCase>();
+  var _areTermsAccepted = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("App terms"),
-      ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
+            padding: EdgeInsets.all(GapSize.xl),
+            child: Center(
               child: Column(
-                  children: <Widget>[
-                    ZOButton(
-                      text: "Potvrdit",
-                      icon: MaterialSymbols.check,
-                      onPressed: () {
-                        _setNewestAcceptedAppTermsUseCase.invoke();
-                        if (mounted) {
-                          context.router.replace(const HomeRoute());
-                        }
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(ZOStrings.certificationCheckPath, width: 205, height: 294),
+                  SizedBox(height: GapSize.xl),
+                  Text(
+                    'Než budete pokračovat,', // FIXME: - Localize
+                    style: TextStyle(fontSize: FontSize.m),
+                  ),
+                  SizedBox(height: GapSize.xs),
+                  Text(
+                    'tak bychom od vás potřebovali souhlas s podmínkami s účastí na projektu.', // FIXME: - Localize
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: FontSize.s),
+                  ),
+                  SizedBox(height: GapSize.m),
+                  ZOCheckbox.rich(
+                      isChecked: _areTermsAccepted,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _areTermsAccepted = value ?? false;
+                        });
                       },
-                    ),
-                  ]
-              )
+                      titleWidget: RichText(
+                          text: TextSpan(
+                              children: [
+                                TextSpan(
+                                    text: 'Souhlasím s ', // FIXME: - Localize
+                                    style: TextStyle(color: Colors.black)
+                                ),
+                                TextSpan(
+                                  text: 'podmínkami účasti na projektu.', // FIXME: - Localize
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      decoration: TextDecoration.underline
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () async {
+                                      await _openUrlInBrowser(ZOStrings.appTerms);
+                                    },
+                                ),
+                              ]
+                          )
+                      )
+                  ),
+                  SizedBox(height: GapSize.l),
+                  ZOButton(
+                    text: 'Pokračovat do aplikace', // FIXME: - Localize
+                    onPressed: _setNewestAcceptedAppTerms,
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _setNewestAcceptedAppTerms() {
+    if (mounted && _areTermsAccepted) {
+      _setNewestAcceptedAppTermsUseCase.invoke();
+      context.router.replace(const HomeRoute());
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        ZOTemporarySnackBar(
+          backgroundColor: Colors.red,
+          message: "Pro pokračování do aplikace musíte nejdříve souhlasit s podmínkami účasti na projektu.", // FIXME: - Localize
+        ),
+      );
+    }
+  }
+
+  Future<void> _openUrlInBrowser(String siteUrl) async {
+    final Uri url = Uri.parse(siteUrl);
+    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      throw Exception('Could not launch $url');
+    }
   }
 }
