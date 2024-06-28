28235b1c490c78b1257775dd27cae2012e40ff8f
Oleksandr Balan <oleksandr.balan@etnetera.cz>
2024-06-28 15:59:39 +0200

    Disable crashlytics for web

--- lib/common/firebase/firebase_helper.dart
+++ lib/common/firebase/firebase_helper.dart
@@ -1,12 +1,14 @@
-import 'dart:ui';
-
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
 /// Helper class for Firebase Crashlytics integration in Flutter.
 class FirebaseHelper {
   /// Initializes Crashlytics for Flutter error handling.
   static void initializeCrashlytics() {
+    if (!isCrashlyticsSupported()) {
+      return;
+    }
+
     FlutterError.onError = (errorDetails) {
       FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
     };
@@ -19,6 +21,21 @@ class FirebaseHelper {
 
   /// Sets the user identifier for Firebase Crashlytics.
   static void setUserIdentifier(String? uid) {
+    if (!isCrashlyticsSupported()) {
+      return;
+    }
+
     FirebaseCrashlytics.instance.setUserIdentifier(uid ?? "");
+  }
+
+  /// Checks if Firebase Crashlytics is supported on the current platform.
+  ///
+  /// This method returns `true` if the application is not running on the web,
+  /// indicating that Firebase Crashlytics can be used. Firebase Crashlytics
+  /// is not currently supported for web platform.
+  ///
+  /// See: https://github.com/firebase/firebase-js-sdk/issues/710
+  static bool isCrashlyticsSupported() {
+    return !kIsWeb;
   }
 }
