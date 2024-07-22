d5474eeae990fe95421d80952abd7581e520dff1
Oleksandr Balan <80771513+oleksandrbalan-etn@users.noreply.github.com>
2024-07-22 10:36:27 +0200

    ZOB-223 Fetch carriers contacts (#122)

--- lib/features/menu/di/menu_dependency_container.dart
+++ lib/features/menu/di/menu_dependency_container.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:zachranobed/features/menu/data/repository/firebase_contacts_repository.dart';
 import 'package:zachranobed/features/menu/domain/repository/contacts_repository.dart';
 import 'package:zachranobed/features/menu/domain/usecase/get_contacts_use_case.dart';
+import 'package:zachranobed/services/carrier_service.dart';
 import 'package:zachranobed/services/configuration_service.dart';
 import 'package:zachranobed/services/entity_pairs_service.dart';
 import 'package:zachranobed/services/entity_service.dart';
@@ -15,6 +16,7 @@ class MenuDependencyContainer {
         GetIt.I<EntityService>(),
         GetIt.I<EntityPairService>(),
         GetIt.I<ConfigurationService>(),
+        GetIt.I<CarrierService>(),
       ),
     );
 
