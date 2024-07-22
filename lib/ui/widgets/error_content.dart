6c1c3795a7df79b44d93febe11b5296a342226af
Oleksandr Balan <80771513+oleksandrbalan-etn@users.noreply.github.com>
2024-07-22 10:47:25 +0200

    ZOB-226 Add loading and error states for contacts screen (#123)

---
+++ lib/ui/widgets/error_content.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:zachranobed/common/constants.dart';
+import 'package:zachranobed/extensions/build_context_extensions.dart';
+import 'package:zachranobed/ui/widgets/button.dart';
+import 'package:zachranobed/ui/widgets/empty_page.dart';
+
+/// A widget that displays an error message with an optional retry button.
+class ErrorContent extends StatelessWidget {
+
+  /// An optional callback to be invoked when the retry button is pressed.
+  final VoidCallback? onRetryPressed;
+
+  const ErrorContent({super.key, required this.onRetryPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        EmptyPage(
+          vectorImagePath: ZOStrings.genericErrorPath,
+          title: context.l10n!.commonGenericErrorTitle,
+          description: context.l10n!.commonGenericErrorDescription,
+        ),
+        if (onRetryPressed != null)
+          ZOButton(
+            text: context.l10n!.commonGenericErrorRetryAction,
+            onPressed: () => onRetryPressed?.call(),
+          ),
+      ],
+    );
+  }
+}
