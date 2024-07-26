f7b3132bcad6020c7503b5cef990c6517250c83f
Jaroslav Novak <jaroslav.novak@etnetera.cz>
2024-07-26 10:26:56 +0200

    feat - Extended device utils

--- lib/common/utils/device_utils.dart
+++ lib/common/utils/device_utils.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:android_id/android_id.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 
 /// Device related utility functions.
 class DeviceUtils {
+  PackageInfo? info;
+
   /// Retrieves device ID for Android or iOS.
   static Future<String?> getId() async {
     switch (Platform.operatingSystem) {
@@ -15,5 +18,10 @@ class DeviceUtils {
       default:
         return null;
     }
+  }
+
+  static Future<String> getAppVersion() async {
+    var info = await PackageInfo.fromPlatform();
+    return "${info.version} (${info.buildNumber})";
   }
 }
