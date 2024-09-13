d561feb278ce3bc973fe0563c65940a782b6cb34
Jiri Zoudun <jiri.zoudun@etnetera.cz>
2024-09-13 15:36:25 +0200

    Finished config (maintain/eslint_upgrade)

--- functions/eslint.config.mjs
+++ functions/eslint.config.mjs
@@ -1,53 +1,53 @@
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import _import from "eslint-plugin-import";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptParser from "@typescript-eslint/parser";
+import eslintPluginTypeScript from "@typescript-eslint/eslint-plugin";
+import eslintPluginImport from "eslint-plugin-import";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
-    ignores: ["lib/**/*"],
-}, ...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
-)), {
-    plugins: {
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
-        import: fixupPluginRules(_import),
-    },
-
+export default [
+  {
+    ignores: ["lib/**/*", "lib/*"],
+  },
+  {
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
-
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "module",
-
-        parserOptions: {
-            tsconfigRootDir: "/Users/jiri.zoudun/Development/flutter/zachranObed/zachranobed/functions",
-        },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: typescriptParser,
+      parserOptions: {
+        tsconfigRootDir: new URL(".", import.meta.url).pathname,
+      },
+      globals: {
+        // Node.js globals
+        require: "readonly",
+        module: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+        URL: "readonly", 
+      },
     },
-
+    plugins: {
+      "@typescript-eslint": eslintPluginTypeScript,
+      "import": eslintPluginImport,
+    },
     rules: {
-        quotes: ["error", "double"],
-        "import/no-unresolved": 0,
-        indent: ["error", 2],
+      // These are the key rules from `eslint:recommended`
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      "no-console": "warn",
+
+      // Google style guide key rules
+      "quotes": ["error", "double"],
+      "require-jsdoc": "off",
+      "valid-jsdoc": "off",
+
+      // TypeScript-specific rules
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+
+      // Import plugin rules
+      "import/no-unresolved": "off",
+      "import/order": ["error", { "groups": [["builtin", "external", "internal"]] }],
+
+      // Your custom rules
+      "indent": ["error", 2],
     },
-}];
+  },
+];
