bcd56b8ada678cb830a09801ae1fbdd1e210d132
Oleksandr Balan <oleksandr.balan@etnetera.cz>
2024-09-13 16:24:56 +0200

    ZOB-273 Fix rebuild issues

--- lib/ui/widgets/screen_content.dart
+++ lib/ui/widgets/screen_content.dart
@@ -8,7 +8,7 @@ import 'package:zachranobed/common/constants.dart';
 /// If the platform is web and the screen width is greater than
 /// [LayoutStyle.webBreakpoint], the [web] child is displayed.
 /// Otherwise, the [mobile] child is displayed.
-class ScreenContent extends StatelessWidget {
+class ScreenContent extends StatefulWidget {
   /// The child to display on the web when the screen width is greater than
   /// [LayoutStyle.webBreakpoint].
   final WidgetBuilder web;
@@ -25,12 +25,31 @@ class ScreenContent extends StatelessWidget {
   });
 
   @override
+  State<ScreenContent> createState() => _ScreenContentState();
+}
+
+/// State for the [ScreenContent] widget.
+///
+/// Manages [_web] and [_mobile] widgets, building them only when needed and
+/// caching them for subsequent use.
+class _ScreenContentState extends State<ScreenContent> {
+  /// The widget to display on the web when the screen width is greater than
+  /// [LayoutStyle.webBreakpoint].
+  Widget? _web;
+
+  /// The widget to display on mobile or when the screen width is less than
+  /// [LayoutStyle.webBreakpoint].
+  Widget? _mobile;
+
+  @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
       if (kIsWeb && constraints.maxWidth > LayoutStyle.webBreakpoint) {
-        return Builder(builder: web);
+        _web ??= widget.web(context);
+        return _web!;
       }
-      return Builder(builder: mobile);
+      _mobile ??= widget.mobile(context);
+      return _mobile!;
     });
   }
 }
