b7ea0344caa58253110e28798d2fbc978c7c639a
Jiri Zoudun <jiri.zoudun@etnetera.cz>
2024-09-30 09:11:12 +0200

    Removed unnecesary code

--- lib/firebase/notifications.dart
+++ lib/firebase/notifications.dart
@@ -105,26 +105,6 @@ class Notifications {
 
   /// Retrieves and saves the FCM token for the device.
   Future<void> getFCMToken() async {
-    NotificationSettings settings = await _firebaseMessaging.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
-
-    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-      ZOLogger.logMessage('User granted permission');
-    } else if (settings.authorizationStatus ==
-        AuthorizationStatus.provisional) {
-      ZOLogger.logMessage('User granted provisional permission');
-    } else {
-      ZOLogger.logMessage('User declined or has not accepted permission');
-      return;
-    }
-
     if (Platform.isIOS) {
       await _firebaseMessaging.getAPNSToken();
     }
