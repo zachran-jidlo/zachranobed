a41f7ab31ab23c1c6a89e24dec56c04c4c77be7a
Jiri Zoudun <jiri.zoudun@etnetera.cz>
2025-07-25 16:26:12 +0200

    Fixed mismatch function. Found out that lack of boxes notification doesn't trigger too. Not sure why, but changing name to a new function helped. Also updated version of functions framework.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,15 @@
 // Import functions
 import { notifyCharityAboutDonationV2 } from "./functions/notifications/foodDeliveryFunction";
 import { notifyCanteenAboutBoxShippmentV2 } from "./functions/notifications/boxReturnFunction";
-import { notifyCharityAboutLackOfBoxesAtCanteenV2 } from "./functions/notifications/lackOfBoxesFunction";
+import { notifyAboutLackOfBoxes } from "./functions/notifications/lackOfBoxesFunction";
 import { boxesMismatchNotification } from "./functions/mismatchFunction";
 import { scheduledFunctionCrontab } from "./functions/checkDeliveriesInvocatorFunction";
-
+import { debugFunction } from "./functions/debugFunction";
 
 // Export for Firebase Functions (CommonJS style)
 exports.notifyCharityAboutDonationV2 = notifyCharityAboutDonationV2;
 exports.notifyCanteenAboutBoxShippmentV2 = notifyCanteenAboutBoxShippmentV2;
-exports.notifyCharityAboutLackOfBoxesAtCanteenV2 =
-  notifyCharityAboutLackOfBoxesAtCanteenV2;
+exports.notifyAboutLackOfBoxes = notifyAboutLackOfBoxes;
 exports.boxesMismatchNotification = boxesMismatchNotification;
 
 exports.scheduledFunctionCrontab = scheduledFunctionCrontab;
