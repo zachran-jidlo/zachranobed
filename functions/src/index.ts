e56b926d1585c58def530333d5b11a7d7db25872
jirizoudun <jiri.zoudun.dev@gmail.com>
2025-07-25 14:11:13 +0200

    ZOB-348: Check boxes counts notification (#235)
    * Implemented schedule function
    
    * Edited readme and added conditional deplyment for schedule to invoke Github Action.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import { notifyCanteenAboutBoxShippmentV2 } from "./functions/notifications/boxR
 import { notifyCharityAboutLackOfBoxesAtCanteenV2 } from "./functions/notifications/lackOfBoxesFunction";
 import { boxesMismatchNotification } from "./functions/mismatchFunction";
 import { scheduledFunctionCrontab } from "./functions/checkDeliveriesInvocatorFunction";
-
+import { monthlyBoxCheckupFunction } from "./functions/notifications/monthlyBoxCheckupFunction";
 
 // Export for Firebase Functions (CommonJS style)
 exports.notifyCharityAboutDonationV2 = notifyCharityAboutDonationV2;
@@ -12,5 +12,13 @@ exports.notifyCanteenAboutBoxShippmentV2 = notifyCanteenAboutBoxShippmentV2;
 exports.notifyCharityAboutLackOfBoxesAtCanteenV2 =
   notifyCharityAboutLackOfBoxesAtCanteenV2;
 exports.boxesMismatchNotification = boxesMismatchNotification;
+exports.monthlyBoxCheckupFunction = monthlyBoxCheckupFunction;
 
-exports.scheduledFunctionCrontab = scheduledFunctionCrontab;
+// Only export the scheduled function for the specific project
+const currentProjectId =
+  process.env.GCLOUD_PROJECT || process.env.FIREBASE_PROJECT_ID;
+const allowedProjectId = "zachran-obed"; // Replace with your target project ID
+
+if (currentProjectId === allowedProjectId) {
+  exports.scheduledFunctionCrontab = scheduledFunctionCrontab;
+}
