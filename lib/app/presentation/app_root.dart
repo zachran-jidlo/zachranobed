f201d637e82b294b93def47adff7b7b60f338a66
Oleksandr Balan <oleksandr.balan@etnetera.cz>
2025-12-05 12:01:06 +0100

    Update typography

--- lib/app/presentation/app_root.dart
+++ lib/app/presentation/app_root.dart
@@ -13,6 +13,7 @@ import 'package:zachranobed/common/presentation/router/app_router.gr.dart';
 import 'package:zachranobed/common/presentation/utils/lifecycle_watcher.dart';
 import 'package:zachranobed/common/presentation/utils/ui_colors.dart';
 import 'package:zachranobed/common/presentation/utils/ui_constants.dart';
+import 'package:zachranobed/common/presentation/utils/ui_text_styles.dart';
 import 'package:zachranobed/features/forceupdate/domain/usecase/check_if_upgrade_app_should_be_shown_usecase.dart';
 import 'package:zachranobed/features/offline/presentation/connectivity_wrapper.dart';
 import 'package:zachranobed/l10n/app_localizations.dart';
@@ -93,28 +94,7 @@ class _AppRootState extends State<AppRoot> with LifecycleWatcher {
               surfaceTint: Colors.white,
               primaryContainer: ZOColors.secondary,
             ),
-            textTheme: Typography.material2021().black.copyWith(
-              // Display
-              displayLarge: const TextStyle(fontSize: 57, height: 64.0 / 57.0, fontWeight: FontWeight.w400),
-              displayMedium: const TextStyle(fontSize: 45, height: 52.0 / 45.0, fontWeight: FontWeight.w400),
-              displaySmall: const TextStyle(fontSize: 36, height: 44.0 / 36.0, fontWeight: FontWeight.w700),
-              // Headline
-              headlineLarge: const TextStyle(fontSize: 32, height: 40.0 / 32.0, fontWeight: FontWeight.w700),
-              headlineMedium: const TextStyle(fontSize: 28, height: 36.0 / 28.0, fontWeight: FontWeight.w400),
-              headlineSmall: const TextStyle(fontSize: 24, height: 32.0 / 24.0, fontWeight: FontWeight.w400),
-              // Title
-              titleLarge: const TextStyle(fontSize: 22, height: 28.0 / 22.0, fontWeight: FontWeight.w700),
-              titleMedium: const TextStyle(fontSize: 16, height: 24.0 / 16.0, fontWeight: FontWeight.w500),
-              titleSmall: const TextStyle(fontSize: 14, height: 20.0 / 14.0, fontWeight: FontWeight.w500),
-              // Body
-              bodyLarge: const TextStyle(fontSize: 16, height: 24.0 / 16.0, fontWeight: FontWeight.w400),
-              bodyMedium: const TextStyle(fontSize: 14, height: 20.0 / 14.0, fontWeight: FontWeight.w400),
-              bodySmall: const TextStyle(fontSize: 12, height: 16.0 / 12.0, fontWeight: FontWeight.w400),
-              // Label
-              labelLarge: const TextStyle(fontSize: 14, height: 20.0 / 14.0, fontWeight: FontWeight.w500),
-              labelMedium: const TextStyle(fontSize: 12, height: 16.0 / 12.0, fontWeight: FontWeight.w500),
-              labelSmall: const TextStyle(fontSize: 11, height: 16.0 / 11.0, fontWeight: FontWeight.w700),
-            ),
+            textTheme: UiTextStyles.getTextTheme(),
             scaffoldBackgroundColor: Colors.white,
             visualDensity: VisualDensity.standard,
             highlightColor: Colors.transparent,
--- lib/common/presentation/utils/ui_text_styles.dart
+++ lib/common/presentation/utils/ui_text_styles.dart
@@ -7,16 +7,18 @@ import 'package:flutter/material.dart';
 class UiTextStyles {
   final TextTheme _theme;
 
-  const UiTextStyles(this._theme);
+  UiTextStyles(this._theme);
 
   TextStyle get displayLarge => _theme.displayLarge ?? const TextStyle();
   TextStyle get displayMedium => _theme.displayMedium ?? const TextStyle();
   TextStyle get displaySmall => _theme.displaySmall ?? const TextStyle();
 
+  TextStyle get headlineHeavy => headlineLarge.copyWith(fontWeight: FontWeight.w700);
   TextStyle get headlineLarge => _theme.headlineLarge ?? const TextStyle();
   TextStyle get headlineMedium => _theme.headlineMedium ?? const TextStyle();
   TextStyle get headlineSmall => _theme.headlineSmall ?? const TextStyle();
 
+  TextStyle get titleHeavy => titleLarge.copyWith(fontWeight: FontWeight.w700);
   TextStyle get titleLarge => _theme.titleLarge ?? const TextStyle();
   TextStyle get titleMedium => _theme.titleMedium ?? const TextStyle();
   TextStyle get titleSmall => _theme.titleSmall ?? const TextStyle();
@@ -28,4 +30,119 @@ class UiTextStyles {
   TextStyle get labelLarge => _theme.labelLarge ?? const TextStyle();
   TextStyle get labelMedium => _theme.labelMedium ?? const TextStyle();
   TextStyle get labelSmall => _theme.labelSmall ?? const TextStyle();
+
+  static TextTheme getTextTheme() {
+    return Typography.material2021().black.copyWith(
+          // Display
+          displayLarge: const TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 57,
+            height: 64.0 / 57.0,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0,
+          ),
+          displayMedium: const TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 45,
+            height: 52.0 / 45.0,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0,
+          ),
+          displaySmall: const TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 36,
+            height: 44.0 / 36.0,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0,
+          ),
+          // Headline
+          headlineLarge: const TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 32,
+            height: 40.0 / 32.0,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0,
+          ),
+          headlineMedium: const TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 28,
+            height: 36.0 / 28.0,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0,
+          ),
+          headlineSmall: const TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 24,
+            height: 32.0 / 24.0,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0,
+          ),
+          // Title
+          titleLarge: const TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 22,
+            height: 28.0 / 22.0,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0,
+          ),
+          titleMedium: const TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 16,
+            height: 24.0 / 16.0,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 0.15,
+          ),
+          titleSmall: const TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 14,
+            height: 20.0 / 14.0,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.1,
+          ),
+          // Body
+          bodyLarge: const TextStyle(
+            fontFamily: 'PlusJakartaSans',
+            fontSize: 16,
+            height: 24.0 / 16.0,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.5,
+          ),
+          bodyMedium: const TextStyle(
+            fontFamily: 'PlusJakartaSans',
+            fontSize: 14,
+            height: 20.0 / 14.0,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.25,
+          ),
+          bodySmall: const TextStyle(
+            fontFamily: 'PlusJakartaSans',
+            fontSize: 12,
+            height: 16.0 / 12.0,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.4,
+          ),
+          // Label
+          labelLarge: const TextStyle(
+            fontFamily: 'PlusJakartaSans',
+            fontSize: 14,
+            height: 20.0 / 14.0,
+            fontWeight: FontWeight.w700,
+            letterSpacing: 0,
+          ),
+          labelMedium: const TextStyle(
+            fontFamily: 'PlusJakartaSans',
+            fontSize: 12,
+            height: 16.0 / 12.0,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0,
+          ),
+          labelSmall: const TextStyle(
+            fontFamily: 'PlusJakartaSans',
+            fontSize: 11,
+            height: 16.0 / 11.0,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0,
+          ),
+        );
+  }
 }
