c74473ba115c177fa48fe79245cfde8d79968d4b
Oleksandr Balan <oleksandr.balan@etnetera.cz>
2026-01-08 16:56:29 +0100

    Redesign DebugScreen and ComponentsScreen

--- lib/features/debug/components_screen.dart
+++ lib/features/debug/components_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:zachranobed/common/presentation/utils/build_context_extensions.dart';
 import 'package:zachranobed/common/presentation/utils/image_assets.dart';
-import 'package:zachranobed/common/presentation/widget/app_bar.dart';
 import 'package:zachranobed/common/presentation/widget/button/ui_button_size.dart';
 import 'package:zachranobed/common/presentation/widget/button/ui_fill_icon_button.dart';
 import 'package:zachranobed/common/presentation/widget/button/ui_icon_button.dart';
@@ -17,6 +16,7 @@ import 'package:zachranobed/common/presentation/widget/donation/ui_donation_time
 import 'package:zachranobed/common/presentation/widget/progress/ui_progress_bar.dart';
 import 'package:zachranobed/common/presentation/widget/progress/ui_progress_stepper.dart';
 import 'package:zachranobed/common/presentation/widget/screen_scaffold.dart';
+import 'package:zachranobed/common/presentation/widget/ui_app_bar.dart';
 import 'package:zachranobed/common/presentation/widget/ui_box_counter_tile.dart';
 import 'package:zachranobed/common/presentation/widget/ui_change_pair_tile.dart';
 import 'package:zachranobed/common/presentation/widget/ui_chip.dart';
@@ -42,7 +42,7 @@ class ComponentsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenScaffold.universal(
-      appBar: const ZOAppBar(
+      appBar: const UiAppBar(
         title: "Components screen",
       ),
       child: CustomScrollView(
--- lib/features/debug/debug_screen.dart
+++ lib/features/debug/debug_screen.dart
@@ -1,15 +1,12 @@
-import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_material_symbols/flutter_material_symbols.dart';
 import 'package:zachranobed/common/domain/utils/zo_logger.dart';
 import 'package:zachranobed/common/presentation/router/app_router.gr.dart';
-import 'package:zachranobed/common/presentation/utils/ui_constants.dart';
-import 'package:zachranobed/common/presentation/widget/app_bar.dart';
-import 'package:zachranobed/common/presentation/widget/button.dart';
 import 'package:zachranobed/common/presentation/widget/button/ui_button_size.dart';
 import 'package:zachranobed/common/presentation/widget/button/ui_primary_button.dart';
 import 'package:zachranobed/common/presentation/widget/screen_scaffold.dart';
+import 'package:zachranobed/common/presentation/widget/ui_app_bar.dart';
 
 @RoutePage()
 class DebugScreen extends StatelessWidget {
@@ -18,28 +15,29 @@ class DebugScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenScaffold.universal(
-      appBar: const ZOAppBar(
+      appBar: const UiAppBar(
         title: "Debug Screen",
       ),
       child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Column(
+            spacing: 16.0,
             children: <Widget>[
-              ZOButton(
+              UiPrimaryButton(
+                size: UiButtonSize.medium(),
                 text: "Test log",
                 icon: MaterialSymbols.bug_report,
                 onPressed: () {
                   ZOLogger.logMessage("This is a debug message to the logger to verify, how the logger works");
                 },
               ),
-              const SizedBox(height: GapSize.xs),
               UiPrimaryButton(
+                size: UiButtonSize.medium(),
                 text: "Components screen",
                 onPressed: () {
                   context.router.push(const ComponentsRoute());
                 },
-                size: UiButtonSize.large(),
               ),
             ],
           ),
