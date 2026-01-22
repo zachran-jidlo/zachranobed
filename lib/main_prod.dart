011de5b8fe631e983e72488eebb1fa5024794dc6
Oleksandr Balan <oleksandr.balan@etnetera.cz>
2026-01-22 15:23:50 +0100

    Remove import from prod flavor

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:zachranobed/app/presentation/app_root.dart';
 import 'package:zachranobed/common/presentation/model/app_flavor_data.dart';
-import 'package:zachranobed/common/presentation/widget/quick_login_button.dart';
 import 'package:zachranobed/main_base.dart';
 
 void main() async {
