3ee5ddd1639b73e7a3f46d0619735527cc3b1e6f
Oleksandr Balan <oleksandr.balan@etnetera.cz>
2026-08-07 15:03:30 +0200

    Filter entity pairs by enabled status and add account activity check

--- lib/common/domain/model/user_data.dart
+++ lib/common/domain/model/user_data.dart
@@ -42,17 +42,27 @@ sealed class UserData {
       Last accepted app terms version: $lastAcceptedAppTermsVersion,
       Active pair: ${activePair.donorId} <-> ${activePair.recipientId},
       Has multiple pairs: $hasMultiplePairs,
+      Account active: $isAccountActive,
       Manual donation enabled: $manualDonationEnabled,
       Tags: $tags,
     ''';
   }
 
+  /// The pairs the user can actually work with.
+  List<EntityPair> get enabledPairs => allPairs.where((pair) => pair.enabled).toList();
+
+  /// Whether at least one pair of this account is turned on.
+  ///
+  /// An account with every pair turned off exists but cannot be used until an
+  /// admin enables a pair.
+  bool get isAccountActive => enabledPairs.isNotEmpty;
+
   /// Determines whether there are multiple pairs available.
-  bool get hasMultiplePairs => allPairs.length > 1;
+  bool get hasMultiplePairs => enabledPairs.length > 1;
 
   /// Checks if any non-active pair requires a food boxes checkup.
   bool get isAnyNonActiveCheckupNeeded {
-    return allPairs
+    return enabledPairs
         .whereNot((pair) => pair.donorId == activePair.donorId && pair.recipientId == activePair.recipientId)
         .any((pair) => getFoodBoxesCheckup(pair).isCheckupNeeded());
   }
